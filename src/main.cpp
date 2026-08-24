471a81e5b21f3b4e6aa91205df79f5987855d3e1
acrinym <acrinym@gmail.com>
2026-08-24 19:37:20 -0400

    Train 14: bump compiler toolchain version

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ struct Cli {
 
 void usage() {
     std::cerr
-        << "Emojineer 0.13\n"
+        << "Emojineer 0.14\n"
         << "usage:\n"
         << "  emojineer repl [--cer registry.json ...]\n"
         << "  emojineer stdlib\n"
