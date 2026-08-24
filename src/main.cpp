1c0aa6c94811e0ae433f05f98c9b78a93792abde
acrinym <acrinym@gmail.com>
2026-08-23 20:38:42 -0400

    Train 10: advance language CLI version

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ struct Cli {
 
 void usage() {
     std::cerr
-        << "Emojineer 0.9\n"
+        << "Emojineer 0.10\n"
         << "usage:\n"
         << "  emojineer repl [--cer registry.json ...]\n"
         << "  emojineer stdlib\n"
