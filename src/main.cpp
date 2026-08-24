921936c8e4fb26c03ee2131690370481b462cebf
acrinym <acrinym@gmail.com>
2026-08-23 22:58:55 -0400

    Train 11: bump emojineer CLI to 0.11

--- src/main.cpp
+++ src/main.cpp
@@ -42,7 +42,7 @@ struct Cli {
 
 void usage() {
     std::cerr
-        << "Emojineer 0.10\n"
+        << "Emojineer 0.11\n"
         << "usage:\n"
         << "  emojineer repl [--cer registry.json ...]\n"
         << "  emojineer stdlib\n"
